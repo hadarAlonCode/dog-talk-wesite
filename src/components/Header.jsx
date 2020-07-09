7f45e3c5a84a3d38c7bdf8c5fb73c54e849cabe1
Hadar Alon <hadaralon3@gmail.com>
2020-07-10 02:39:39 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import React from "react"
 import dog from "../images/dog-h.png"
 import logo from "../images/logo1.png"
 import border from "../images/customWhiteBorder.png"
+import Fade from 'react-reveal/Fade';
 
 const Header = (props) => (
   <header>
@@ -12,16 +13,21 @@ const Header = (props) => (
     <div className="header__border"></div>
 
            <section className="img__section">
+           <Fade left delay={200}>
                <img className='header__image'src={dog} alt="dog" />
+               </Fade>
            </section>
            <section className="text__section">
               {/* <h1>Dog Talk</h1> */}
+              <Fade right delay={200}>
               <img className='logo__image'src={logo} alt="Dog Talk" />
-
+              </Fade>
+              <Fade right  delay={200}>
               <h1>אילוף כלבים, מעון יום, פנסיון והכשרת כלבי ביטחון</h1>
               <a style={{textDecoration: "none"}} href="tel:+972543978591" >
                   <button className="btn">התקשרו עכשיו</button>
                   </a>
+                  </Fade>
 
            </section>
     </div>
